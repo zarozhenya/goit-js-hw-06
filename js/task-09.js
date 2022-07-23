abfe8c0e7a57124bd4d623a9e93b523566e2e097
Yevhen <eug.z2004@gmail.com>
2022-07-23 14:17:30 +0300

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  colorText: document.querySelector(".widget  .color"),
+  changeButton: document.querySelector(".widget > .change-color"),
+};
+
+refs.changeButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.colorText.textContent = color;
+});
