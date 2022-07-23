98964159fc06c8f7082460930db29c06c8eb31f3
Yevhen <eug.z2004@gmail.com>
2022-07-23 14:00:17 +0300

    task7 done

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const refs = {
+  input: document.querySelector("#font-size-control"),
+  output: document.querySelector("#text"),
+};
+refs.output.style.fontSize = refs.input.value + "px";
+console.log(refs.output.style);
+refs.input.addEventListener("input", (event) => {
+  refs.output.style.fontSize = event.currentTarget.value + "px";
+});
