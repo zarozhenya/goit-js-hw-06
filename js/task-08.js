ec29808ab888aa89ab5bdf2eeff1af5b2f7512a3
Yevhen <eug.z2004@gmail.com>
2022-07-23 14:11:59 +0300

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be filled");
+  } else {
+    const fieldsObj = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(fieldsObj);
+    event.currentTarget.reset();
+  }
+});
