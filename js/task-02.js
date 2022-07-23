9d279477493a1c2cfe6ae480a035f26fd2f61920
Yevhen <eug.z2004@gmail.com>
2022-07-23 12:59:03 +0300

    task2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.querySelector("#ingredients");
+
+const itemsRef = ingredients.map((ingredient) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = ingredient;
+  itemRef.classList.add("item");
+  return itemRef;
+});
+
+listRef.append(...itemsRef);
